40dfb06a389f9b7e30494b6322b3f91b098a9b62
wendyPop <wishbong@gmail.com>
2022-07-31 10:27:49 +0900

    first

---
+++ src/component/list/CommentList.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import CommentListItem from "./CommentListItem";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  
+  & > * {
+    :not(:last-child) {
+      margin-bottom: 16px;
+    }
+  }
+`
+
+function CommentList(props) {
+    const { comments } = props
+    return (
+        <Wrapper>
+            {
+                comments && comments.map((comment) => {
+                    return <CommentListItem key={comment.id} comment={comment} />
+                })
+            }
+        </Wrapper>
+    )
+}
+
+export default CommentList
